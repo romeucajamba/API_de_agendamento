8846f44d62c6b93aae63201b90c883f07588c2d2
RomeuCajamba <romeucajamba@gmail.com>
2024-03-04 11:48:46 +0100

    Criação da rota do user

--- src/useCases/use.cases.ts
+++ src/useCases/use.cases.ts
@@ -1,5 +1,14 @@
+import { UserCreate, UserRepository } from "../interface/user.interface";
+import { UserRepositoryPrisma } from "../repositories/user.respository";
+
 class UseCase {
+    private userRepository: UserRepository;
+
     constructor(){
-        
+        this.userRepository = new UserRepositoryPrisma()
+    }
+
+    async create({name, email}: UserCreate): Promise<User>{
+
     }
 }
